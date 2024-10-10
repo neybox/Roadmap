a168b99a9558c9b1b1da1e1779071ef72cf8a1f1
dickieee <dickie@neybox.com>
2024-10-10 10:45:18 +0100

    Updated edge insets

--- Sources/Roadmap/RoadmapView.swift
+++ Sources/Roadmap/RoadmapView.swift
@@ -63,7 +63,7 @@ public struct RoadmapView<Header: View, Footer: View>: View {
 				ForEach(viewModel.filteredFeatures) { feature in
 					RoadmapFeatureView(viewModel: viewModel.featureViewModel(for: feature))
 						.macOSListRowSeparatorHidden()
-						.listRowInsets(EdgeInsets())
+						.listRowInsets(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
 						.listRowBackground(Color.clear)
 				}
 				footer
