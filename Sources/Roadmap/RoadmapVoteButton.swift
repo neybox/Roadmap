3f2043c6574535d213bbfd847c809783691b9800
dickieee <dickie@neybox.com>
2024-10-24 14:17:32 +0100

    Made the entire label view hierarchy completely inaccessible

--- Sources/Roadmap/RoadmapVoteButton.swift
+++ Sources/Roadmap/RoadmapVoteButton.swift
@@ -34,78 +34,7 @@ struct RoadmapVoteButton: View {
                 }
             }
         } label: {
-            ZStack {
-                if typeSize.isAccessibilitySize {
-                    HStack(spacing: isHovering ? 2 : 0) {
-                        if viewModel.canVote {
-                            if !viewModel.hasVoted {
-                                viewModel.configuration.style.upvoteIcon
-									.accessibility(hidden: true)
-                                    .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                    .imageScale(.large)
-                                    .font(Font.system(size: 17, weight: .medium))
-                                    .frame(maxWidth: 24, maxHeight: 24)
-                            } else {
-                                viewModel.configuration.style.unvoteIcon
-									.accessibility(hidden: true)
-                                    .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                    .imageScale(.large)
-                                    .font(Font.system(size: 17, weight: .medium))
-                                    .frame(maxWidth: 24, maxHeight: 24)
-                            }
-                        }
-                        
-                        if showNumber {
-                            Text("\(viewModel.voteCount)")
-								.accessibility(hidden: true)
-                                .lineLimit(1)
-                                .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                .minimumScaleFactor(0.5)
-                                .font(viewModel.configuration.style.numberFont)
-                        }
-                    }
-					.accessibility(hidden: true)
-                    .padding(viewModel.configuration.style.radius)
-                    .frame(minHeight: 64)
-                    .background(backgroundView)
-                } else {
-                    VStack(spacing: isHovering ? 6 : 4) {
-                        if viewModel.canVote {
-                            if !viewModel.hasVoted {
-                                viewModel.configuration.style.upvoteIcon
-									.accessibility(hidden: true)
-                                    .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                    .imageScale(.large)
-                                    .font(viewModel.configuration.style.numberFont)
-                                    .frame(maxWidth: 20, maxHeight: 20)
-                                    .minimumScaleFactor(0.75)
-                            } else {
-                                viewModel.configuration.style.unvoteIcon
-									.accessibility(hidden: true)
-                                    .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                    .imageScale(.large)
-                                    .font(viewModel.configuration.style.numberFont)
-                                    .frame(maxWidth: 20, maxHeight: 20)
-                                    .minimumScaleFactor(0.75)
-                            }
-                        }
-                        
-                        if showNumber {
-                            Text("\(viewModel.voteCount)")
-								.accessibility(hidden: true)
-                                .lineLimit(1)
-                                .foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
-                                .font(viewModel.configuration.style.numberFont)
-                                .minimumScaleFactor(0.9)
-                        }
-                    }
-                    .frame(minWidth: 56)
-                    .frame(height: 64)
-                    .background(backgroundView)
-                }
-            }
-            .contentShape(RoundedRectangle(cornerRadius: viewModel.configuration.style.radius, style: .continuous))
-            .overlay(overlayBorder)
+			buttonLabel
         }
 		.debounce(isDebouncing: $isDebouncing, for: 1.0)
 		.disabled(!viewModel.canVote || isDebouncing || !viewModel.configuration.voter.canVote(for: viewModel.feature))
@@ -143,11 +72,81 @@ struct RoadmapVoteButton: View {
 		.accessibilityLabel(viewModel.canVote ? !viewModel.hasVoted ? "Upvote" : "Downvote" : "")
 		.accessibilityAddTraits(.isButton)
         //.animateAccessible()
-        .accessibilityShowsLargeContentViewer()
+        //.accessibilityShowsLargeContentViewer()
     }
+	
+	@ViewBuilder
+	private var buttonLabel: some View {
+		ZStack {
+			if typeSize.isAccessibilitySize {
+				HStack(spacing: isHovering ? 2 : 0) {
+					if viewModel.canVote {
+						if !viewModel.hasVoted {
+							viewModel.configuration.style.upvoteIcon
+								.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+								.imageScale(.large)
+								.font(Font.system(size: 17, weight: .medium))
+								.frame(maxWidth: 24, maxHeight: 24)
+						} else {
+							viewModel.configuration.style.unvoteIcon
+								.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+								.imageScale(.large)
+								.font(Font.system(size: 17, weight: .medium))
+								.frame(maxWidth: 24, maxHeight: 24)
+						}
+					}
+					
+					if showNumber {
+						Text("\(viewModel.voteCount)")
+							.lineLimit(1)
+							.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+							.minimumScaleFactor(0.5)
+							.font(viewModel.configuration.style.numberFont)
+					}
+				}
+				.padding(viewModel.configuration.style.radius)
+				.frame(minHeight: 64)
+				.background(backgroundView)
+			} else {
+				VStack(spacing: isHovering ? 6 : 4) {
+					if viewModel.canVote {
+						if !viewModel.hasVoted {
+							viewModel.configuration.style.upvoteIcon
+								.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+								.imageScale(.large)
+								.font(viewModel.configuration.style.numberFont)
+								.frame(maxWidth: 20, maxHeight: 20)
+								.minimumScaleFactor(0.75)
+						} else {
+							viewModel.configuration.style.unvoteIcon
+								.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+								.imageScale(.large)
+								.font(viewModel.configuration.style.numberFont)
+								.frame(maxWidth: 20, maxHeight: 20)
+								.minimumScaleFactor(0.75)
+						}
+					}
+					
+					if showNumber {
+						Text("\(viewModel.voteCount)")
+							.lineLimit(1)
+							.foregroundColor(hasVoted ? viewModel.configuration.style.selectedForegroundColor : viewModel.configuration.style.tintColor)
+							.font(viewModel.configuration.style.numberFont)
+							.minimumScaleFactor(0.9)
+					}
+				}
+				.frame(minWidth: 56)
+				.frame(height: 64)
+				.background(backgroundView)
+			}
+		}
+		.contentShape(RoundedRectangle(cornerRadius: viewModel.configuration.style.radius, style: .continuous))
+		.overlay(overlayBorder)
+		.accessibilityHidden(true)  // Make the entire label hierarchy inaccessible
+	}
     
     @ViewBuilder
-    var overlayBorder: some View {
+    private var overlayBorder: some View {
         if isHovering {
             RoundedRectangle(cornerRadius: viewModel.configuration.style.radius, style: .continuous)
                 .stroke(viewModel.configuration.style.tintColor, lineWidth: 1)
